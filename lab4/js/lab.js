ed6311a891ec6b021a4e5cdca9a728dd92b3c6b3
JacquelineCastro <jcastr30@ucsc.edu>
2022-04-16 00:37:24 -0700

    Lab4 w/o Screenshots/Images

---
+++ lab4/js/lab.js
@@ -0,0 +1,18 @@
+/*  Lab4: Pseudocoding & Problem-Solving.
+ *  Add a coumpter task pseudocode to a javascript file in comments.
+ *  @link   URL(none)
+ *  @lab.js
+ *  @Jacqueline Casttro
+ *  @04/152022
+ */
+
+/* Creating a Flappy Bird Clone:
+      1.Print the background
+      2.Randomize Obstacles (has a set range)
+      3.Add player
+      4.Accept user Input (taps)
+      5.Evaluate movement
+      6.Start Score
+      7.Check for Player's hitbox
+      8.If Hitbox touches obstacle, end, otherwise, loop check for hitbox.
+*/
