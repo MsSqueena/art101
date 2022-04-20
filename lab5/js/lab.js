a345f9fd885f3f6a43a3b68800fd972b9bb0b962
JacquelineCastro <jcastr30@ucsc.edu>
2022-04-20 10:54:06 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/*
+  Author: Jacqueline Castro
+  Created: 04/20/2022
+  Lab 5: Task 2
+*/
+
+//Car Information
+var make = "Kia";
+var model = "Seltos";
+var color = "Steel Grey";
+var year = 2020;
+var currentYear = 2022;
+var carAge = currentYear - year;
+
+//Boolean Value
+let ownIt = true;
+
+//Print Out Information
+document.writeln("Make: "+make+"<br>");
+document.writeln("Model: "+model+"<br>");
+document.writeln("Color: "+color+"<br>");
+document.writeln("Year: "+year+"<br>");
+document.writeln("Age: "+carAge+"<br>");
+document.writeln("Yours?: "+ownIt+"<br>");
