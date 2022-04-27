f537e1e453bce0ea744146f10667e442544388a7
JacquelineCastro <jcastr30@ucsc.edu>
2022-04-26 18:26:31 -0700

    lab 7

---
+++ lab7/js/lab7.js
@@ -0,0 +1,26 @@
+/*
+Author: Jacqueline Castro
+Created: 04/26/2022
+Lab 7: Functions
+*/
+
+//For this function, we are going to short the letters in someone's name.
+function nameSorter(x){
+  //Get's user's input & add a little comment
+  document.writeln("<b>"+x+"</b> hu? That's a pretty cool name! <br>");
+  document.writeln("I hope you don't mind but I'm going to switch the letters in your name around a bit.<br>");
+
+  //Start by making the name into an array and printing out the result
+  var split = x.split('');
+  document.writeln("First, we got to split up the letters in your name: <b>"+split+"</b><br>");
+  //then we sort the stings in that array and join them
+  split.sort();
+  var joined = split.join('');
+
+  //then add another little comment and return the value so it can print.
+  document.writeln("And then we're going to rearrange them in alphabetical order. . . <br>");
+  return joined;
+}
+//print the function
+var userName = window.prompt("What's your name?");
+document.writeln("And tada! Your name is now <b>"+nameSorter(userName)+"</b><br>");
